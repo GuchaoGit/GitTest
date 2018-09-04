95961a8705bb3bb559a687d284208e8fd7053347
guchaochao <guchaochao@huiyunit.com>
2018-09-04 13:54:27 +0800

    dev

--- app/src/main/java/com/guc/gittest/MainActivity.java
+++ app/src/main/java/com/guc/gittest/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //master分支
+        //新建dev分支
 
     }
 }
